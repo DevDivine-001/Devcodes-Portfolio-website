a77c04595dfc6086986053a09dc05fe75da95240
Oscdivin <divineuwandu84@Gmail.com>
2024-10-02 16:27:21 +0100

    Testimonial

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import "./Testimonial.scss"
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi'
+import { useState } from 'react'
+import { client, urlFor } from "../../clients"
+import { AppWrap, MotionWrap } from "../../wrapper";
+import { motion } from "framer-motion"
 
 const Testimonial = () => {
+    const [brands, setBrands] = useState([]);
+    const [testimonial, setTestimonial] = useState([]);
     return (
         <div>Testimonial</div>
     )
